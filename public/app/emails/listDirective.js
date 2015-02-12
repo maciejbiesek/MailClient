afe0e915adac2a71178acdfa476a2161aabab5f5
Pola <mikolajczak.pola@gmail.com>
2015-02-12 11:58:43 +0100

    list witch search

--- public/app/emails/listDirective.js
+++ public/app/emails/listDirective.js
@@ -6,18 +6,39 @@ angular.module('MailClient.app.emails.listDirective', [])
 			replace: true,
 			scope: {
 				emails: '=',
+				searchTerm: '='
 			},
 			template: '<ul id = "list"></ul>',
 			link: function (scope, element, attrs) {
 				var list = document.getElementById('list');
-				scope.$watch('emails', function(newValue) {
+				scope.$watchGroup(['emails','searchTerm'], function(newValue) {
+					list.innerHTML = "";
+					console.log(newValue);
+					var searchTerm = newValue[1];
+					var searchBy = ["title", "content", "sender"];
+					var display;
+					var messageToDisplay;
+					var reg = new RegExp("(" + searchTerm + ")","gim");
 					if (newValue !== undefined) {
-						scope.emails.forEach(function(item) {
+						newValue[0].forEach(function(message) {
+							display = searchTerm==null;
+							messageToDisplay = {};
 							var li = document.createElement("li");
-							if (item.read === false) {
+							if (message.read === false) {
 								li.setAttribute("class", "unread");
 							}
-							li.innerHTML = item.title;
+							for (var j in searchBy){
+								var matches = reg.test(message[searchBy[j]]);
+								console.log(matches)
+								if (matches){
+									display = true;
+								}
+								messageToDisplay[searchBy[j]] = message[searchBy[j]].replace(reg, '<strong>$1</strong>');
+							}
+							if (!display){
+								li.classList.add("hidden");
+							}
+							li.innerHTML = messageToDisplay.title;
 							list.appendChild(li);
 						});
 					}	
