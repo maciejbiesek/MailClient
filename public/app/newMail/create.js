f36079f1a8c81bf80a2118a93e9852282198ff89
Kasia <kasia@kasia-K50ID.(none)>
2015-02-16 17:41:11 +0100

    form for creating new mail

--- public/app/newMail/create.js
+++ public/app/newMail/create.js
@@ -1,5 +1,32 @@
 angular.module('MailClient.app.newMail.CreateCtrl', [])
 
-    .controller('CreateCtrl', function () {
-        console.log('create');
+    .controller('CreateCtrl', function ($scope, $http, $state) {
+    	$scope.receiver = '';
+    	$scope.title = '';
+        $scope.content = '';
+        $scope.receivers = [];
+
+        $scope.addReceiver = function (receiver) {
+        	if (receiver) {
+        		$scope.receivers.push(receiver);
+        		$scope.receiver = '';
+        	}
+        };
+
+        $scope.send = function () {
+        	if ($scope.receiver) {
+        		$scope.addReceiver($scope.receiver);
+        	}
+        	if($scope.receivers && $scope.title && $scope.content) {
+        		$http.post('/sent', {
+        			id: "" + Date.now(),
+        			title: $scope.title,
+        			content: $scope.content,
+        			receivers: $scope.receivers,
+        			sent: Date.now()
+        		}).success(function (res) {
+        			$state.go('main.sent');
+        		});
+        	}
+        };
     });
