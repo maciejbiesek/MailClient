88ea89b788f7d9a844ff123031a35118924e992e
Pola <mikolajczak.pola@gmail.com>
2015-02-17 22:34:59 +0100

    merge fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt) {
             },
             files: [
               '<%= paths.public %>/**/{,*/}*.js',
-              '!<%= paths.public %>/libraries/*'
+              '!<%= paths.public %>/libraries/*',
+              '!<%= paths.public %>/components/*'
             ]
         },
 
@@ -36,14 +37,16 @@ module.exports = function(grunt) {
                     src: [
                         '<%= paths.public %>/**/{,*/}*.js',
                         '!<%= paths.public %>/main.js',
-                        '!<%= paths.public %>/libraries/*'
+                        '!<%= paths.public %>/libraries/*',
+                        '!<%= paths.public %>/components/*'
                     ],
                     dest: '<%= paths.public %>/main.js'
                 },
                 css: {
                     src: [
                         '<%= paths.public %>/**/{,*/}*.css',
-                        '!<%= paths.public %>/main.css'
+                        '!<%= paths.public %>/main.css',
+                        '!<%= paths.public %>/components/*'
                     ],
                     dest: '<%= paths.public %>/main.css'
                 }
@@ -58,7 +61,7 @@ module.exports = function(grunt) {
             livereload: {
                 options: {
                     open: {
-                        target: 'http://localhost:3000',
+                        target: 'http://localhost:3000/#/inbox',
                         appName: 'google-chrome'
                     },
                     base: [
@@ -73,7 +76,8 @@ module.exports = function(grunt) {
                 files: [
                     '<%= paths.public %>/**/{,*/}*.js',
                     '!<%= paths.public %>/main.js',
-                    '!<%= paths.public %>/libraries/*'
+                    '!<%= paths.public %>/libraries/*',
+                    '!<%= paths.public %>/components/*'
                 ],
                 tasks: [
                     'jshint',
@@ -84,7 +88,8 @@ module.exports = function(grunt) {
             css: {
                 files: [
                     '<%= paths.public %>/**/{,*/}*.css',
-                    '!<%= paths.public %>/main.css'
+                    '!<%= paths.public %>/main.css',
+                    '!<%= paths.public %>/components/*'
                 ],
                 tasks: [
                     'concat:css'
